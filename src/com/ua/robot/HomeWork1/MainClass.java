e7d4cfe5abdf71d1cca02619a35fad1864fe908d
Diana Moskalenko <comdiana90@gmail.com>
2023-01-26 18:10:06 +0200

    Initial commit

---
+++ src/com/ua/robot/HomeWork1/MainClass.java
@@ -0,0 +1,8 @@
+package com.ua.robot.HomeWork1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+
+    }
+}
---
+++ src/com/ua/robot/HomeWork2/IntegerClass.java
@@ -0,0 +1,13 @@
+package com.ua.robot.HomeWork2;
+
+public class IntegerClass {
+    public static void main(String[] args) {
+        System.out.println("Введемо дві змінні та виконаємо з ними дії");
+        int a = 100;
+        int b = 10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
