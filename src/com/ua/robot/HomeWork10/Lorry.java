cf416f5bded565dcc36a03716828659f7d1493a6
Diana Moskalenko <comdiana90@gmail.com>
2023-02-20 12:06:42 +0200

    HomeWork 10 Done

---
+++ src/com/ua/robot/HomeWork10/Lorry.java
@@ -0,0 +1,10 @@
+package com.ua.robot.HomeWork10;
+
+public class Lorry extends Car {
+
+    @Override
+    protected void Type() {
+        System.out.println("Lorry");
+    }
+
+}
---
+++ src/com/ua/robot/HomeWork10/PassengerCar.java
@@ -0,0 +1,13 @@
+package com.ua.robot.HomeWork10;
+
+public class PassengerCar extends Car {
+
+    @Override
+    protected void Type() {
+        System.out.println("Passenger Car");
+    }
+
+
+
+
+}
