0d70976b6f54458a5bfc53d919213cfc3740ec4e
Diana Moskalenko <comdiana90@gmail.com>
2023-03-23 21:12:45 +0200

    HomeWork 15 Done

---
+++ src/com/ua/robot/HomeWork16/Main.java
@@ -0,0 +1,22 @@
+package com.ua.robot.HomeWork16;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Set<Integer> hs = new HashSet<>();
+        Set<Integer> lhs = new LinkedHashSet<>();
+        Set<Integer> ts = new TreeSet<>();
+
+        for (int i = 0; i < 1000; i++) {
+            hs.add(random.nextInt(1, 51));
+            lhs.add(random.nextInt(1, 51));
+            ts.add(random.nextInt(1, 51));
+        }
+
+        System.out.println(hs);
+        System.out.println(lhs);
+        System.out.println(ts);
+    }
+}
