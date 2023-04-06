06f842483fcc0bdedebbb7db4973057b28ead8cc
Diana Moskalenko <comdiana90@gmail.com>
2023-04-06 10:28:35 +0300

    HomeWork 19 Done

--- src/com/ua/robot/HomeWork19/Main.java
+++ src/com/ua/robot/HomeWork19/Main.java
@@ -3,6 +3,7 @@ package com.ua.robot.HomeWork19;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -32,5 +33,13 @@ public class Main {
         Map<String, Long> sort = lastName.stream().collect(Collectors.groupingBy(String::valueOf, Collectors.counting()));
         System.out.println(sort + "\n");
 
+        Random random = new Random();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(random.nextInt(10));
+        }
+        System.out.println(numbers);
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("sum = " + sum);
     }
 }
